c0b46e772504f9b41d072572080debe8108a40d3
James <james_w_kennedy@outlook.com>
2022-12-13 22:27:48 -0600

    finished round creation logic

--- TrackerLibrary/DataAccess/SQLConnector.cs
+++ TrackerLibrary/DataAccess/SQLConnector.cs
@@ -83,6 +83,7 @@ namespace TrackerLibrary.DataAccess
                 SaveTournament(model, conn);
                 SaveTournamentPrizes(model, conn);
                 SaveTournamentEntries(model, conn);
+                SaveTournamentRounds(model, conn);  
             }
         }
         private void SaveTournament(TournamentModel model, IDbConnection conn)
@@ -120,6 +121,35 @@ namespace TrackerLibrary.DataAccess
                 conn.Execute("dbo.spTournamentEntries_Insert", p, commandType: CommandType.StoredProcedure);
             }
 
+        }
+
+        private void SaveTournamentRounds(IDbConnection connection, TournamentModel model)
+        {
+            foreach (List<MatchupModel> round in model.Rounds)
+            {
+                foreach (MatchupModel matchup in round)
+                {
+                    var p = new DynamicParameters();
+                    p.Add("@TournamentId", model.Id);
+                    p.Add("@MatchupRound", matchup.MatchupRound);
+                    p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                    connection.Execute("dbo.spMatchups_Insert", p, commandType: CommandType.StoredProcedure);
+
+                    matchup.Id = p.Get<int>("@id");
+
+                    foreach (MatchupEntryModel entry in matchup.Entries)
+                    {
+                        p = new DynamicParameters();
+                        p.Add("@MatchupId", matchup.Id);
+                        p.Add("@ParentMatchupId", entry.ParentMatchup);
+                        p.Add("@TeamCompeting", entry.TeamCompeting.Id);
+                        p.Add("@id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                        connection.Execute("dbo.spMatchupEntries_Insert", p, commandType: CommandType.StoredProcedure);
+                    }
+                }
+            }
         }
         public List<PersonModel> GetPerson_All()
         {
